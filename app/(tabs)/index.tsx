a2fb6a73572d0fefdfacd115568b1ae65f18c49d
ArionDutta33 <ariondutta333@outlook.com>
2024-11-03 10:28:13 +0530

    Loading the next images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,61 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
 
-import { ScreenContent } from '~/components/ScreenContent';
+import { useEffect, useState } from 'react';
+import { FlatList, Text } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function Home() {
+  const [persmissionResponse, requestPermission] = MediaLibrary.usePermissions();
+  const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [endCursor, setEndCursor] = useState<string>();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    //!!!!og
+    if (persmissionResponse?.status !== 'granted') {
+      requestPermission();
+    }
+  }, []);
+  useEffect(() => {
+    if (persmissionResponse?.status === 'granted') {
+      loadLocalAssets();
+    }
+  }, [persmissionResponse]);
+
+  const loadLocalAssets = async () => {
+    if (loading) return;
+    setLoading(true);
+    const assetsPage = await MediaLibrary.getAssetsAsync({
+      after: endCursor,
+    });
+    console.log(assetsPage);
+    setLocalAssets((existingItems) => [...existingItems, ...assetsPage.assets]);
+    setHasNextPage(assetsPage.hasNextPage);
+    setEndCursor(assetsPage.endCursor);
+    setLoading(false);
+  };
+
+  console.log(JSON.stringify(persmissionResponse, null, 2));
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <Stack.Screen options={{ title: 'Photos' }} />
+      <FlatList
+        data={localAssets}
+        numColumns={4}
+        contentContainerClassName="gap-[2px]"
+        columnWrapperClassName="gap-1"
+        // columnWrapperStyle={{ gap: 2 }}
+        // contentContainerStyle={{ padding: 2 }}
+        renderItem={({ item }) => (
+          <Image style={{ width: '25%', aspectRatio: 1 }} source={{ uri: item.uri }} />
+        )}
+      />
+      {hasNextPage && (
+        <Text onPress={loadLocalAssets} className="p-2 text-center">
+          Load next
+        </Text>
+      )}
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
